8901b5b21b16abf481783888b4102de24246a392
Richard Friedman <rlf613@me.com>
2019-05-20 18:52:43 +1000

    Initial commit

---
+++ project3/project3/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  project3
+//
+//  Created by Richard Friedman on 20/5/19.
+//  Copyright © 2019 grp140. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
