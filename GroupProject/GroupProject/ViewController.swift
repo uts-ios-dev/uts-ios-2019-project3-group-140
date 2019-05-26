040fc9972ba08e3bb51857165b50e7784a586add
Yamin Khin <31737365+GannieD@users.noreply.github.com>
2019-05-26 14:40:46 +1000

    Add files via upload
    Unknown error on Project3, working on GroupProject

---
+++ GroupProject/GroupProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupProject
+//
+//  Created by 张梦凡 on 26/5/19.
+//  Copyright © 2019 UTS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
